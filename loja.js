b9309b687839e419a6a3f9b8cd8cfe3255f6c04d
Cleiton Cardoso <cleiton_kssa@hotmail.com>
2022-08-09 20:44:08 -0300

    arquivo loja

---
+++ loja.js
@@ -0,0 +1,10 @@
+function Loja(loja, site, endereco, cnpj, telefone, email) {
+  this.nome = loja;
+  this.site = site;
+  this.endereco = endereco;
+  this.cnpj = cnpj;
+  this.telefone = telefone;
+  this.email = email;
+}
+module.exports = Loja;
+
