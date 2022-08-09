c55952441db8210100803d1410d5d38dd73c8e8b
Rodrigo Lotaif <rodrigolotaif@gmail.com>
2022-08-09 20:28:22 -0300

    Cliente

--- Cliente.js
+++ Cliente.js
@@ -1,6 +1,16 @@
-function Cliente(nome, idade, endereco, telefone ){
+function cliente(nome, idade, endereco, telefone, email){
+    nome, 
+    idade,
+    endereco,
+    telefone,
+    email,
+
+
     this.nome 
     this.idade  
     this.endereco 
+    this.email
     this.telefone 
     }
+
+    module.exports = cliente
