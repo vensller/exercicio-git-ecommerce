91d800d0a007c2d2bad15823dc2eba8a8e557bc7
Rodrigo Lotaif <rodrigolotaif@gmail.com>
2022-08-09 21:24:43 -0300

    cliente com funcionalidade cliente/pedidos

--- Cliente.js
+++ Cliente.js
@@ -4,12 +4,14 @@ function cliente(
     endereco,
     telefone,
     email,
+    pedidos,
     ){
     this.nome = nome
     this.idade = idade 
     this.endereco = endereco
     this.email = email
     this.telefone = telefone
+    this.pedidos = pedidos
     }
 
     module.exports = cliente
